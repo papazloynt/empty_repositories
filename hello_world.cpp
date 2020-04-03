3538aa4970702a541119dfb6c07f38db28bb5fae
papazloynt <cool.chastikov53@gmail.com>
2020-04-03 19:30:58 +0300

    MOdificate with //

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,9 @@
 
 int main() 
 {
-char* a=nullptr;
-std::cout<<"INput your name"<<std::endl;
-std::cin>>a;
-std::cout<<"Hello world from "<<a<<std::endl;
+char* a=nullptr;          //Создаём переменную
+std::cout<<"INput your name"<<std::endl; 
+std::cin>>a; //Вводим имя
+std::cout<<"Hello world from "<<a<<std::endl; //Печатаем
 }
 return 0;
